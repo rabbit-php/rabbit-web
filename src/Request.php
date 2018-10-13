3c151c5297105153d9cac6a46d88fee873860cea
albert <63851587@qq.com>
2018-10-13 10:42:32 +0800

    update

---
+++ src/Request.php
@@ -0,0 +1,401 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/10/9
+ * Time: 13:34
+ */
+
+namespace rabbit\web;
+
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UploadedFileInterface;
+use Psr\Http\Message\UriInterface;
+
+class Request implements ServerRequestInterface
+{
+    use MessageTrait;
+    /**
+     * @var \Swoole\Http\Request
+     */
+    protected $swooleRequest;
+
+    /**
+     * @var array
+     */
+    private $attributes = [];
+
+    /**
+     * @var array
+     */
+    private $cookieParams = [];
+
+    /**
+     * @var null|array|object
+     */
+    private $parsedBody;
+
+    /**
+     * @var array
+     */
+    private $queryParams = [];
+
+    /**
+     * @var array
+     */
+    private $serverParams = [];
+
+    /**
+     * @var array
+     */
+    private $uploadedFiles = [];
+
+    /**
+     * @var string
+     */
+    private $method;
+
+    /**
+     * @var UriInterface|Uri
+     */
+    private $uri;
+
+    public function __construct(\Swoole\Http\Request $swooleRequest)
+    {
+        $server = $swooleRequest->server;
+        $this->method = strtoupper($server['request_method'] ?? 'GET');
+        $this->setHeaders($swooleRequest->header ?? []);
+        $this->uri = self::getUriFromGlobals($swooleRequest);
+        $body = new SwooleStream($swooleRequest->rawContent());
+        $this->protocol = isset($server['server_protocol']) ? str_replace('HTTP/', '', $server['server_protocol']) : '1.1';
+
+        $this->stream = $body;
+
+        $this->withCookieParams($swooleRequest->cookie ?? [])
+            ->withQueryParams($swooleRequest->get ?? [])
+            ->withParsedBody($swooleRequest->post ?? [])
+            ->withUploadedFiles(self::normalizeFiles($swooleRequest->files ?? []))
+            ->withServerParams($server ?? [])
+            ->setSwooleRequest($swooleRequest);
+    }
+
+    private static function normalizeFiles(array $files)
+    {
+        $normalized = [];
+
+        foreach ($files as $key => $value) {
+            if ($value instanceof UploadedFileInterface) {
+                $normalized[$key] = $value;
+            } elseif (is_array($value) && isset($value['tmp_name'])) {
+                $normalized[$key] = self::createUploadedFileFromSpec($value);
+            } elseif (is_array($value)) {
+                $normalized[$key] = self::normalizeFiles($value);
+                continue;
+            } else {
+                throw new \InvalidArgumentException('Invalid value in files specification');
+            }
+        }
+
+        return $normalized;
+    }
+
+    private static function createUploadedFileFromSpec(array $value)
+    {
+        if (is_array($value['tmp_name'])) {
+            return self::normalizeNestedFileSpec($value);
+        }
+
+        return new UploadedFile($value['tmp_name'], (int)$value['size'], (int)$value['error'], $value['name'], $value['type']);
+    }
+
+    private static function normalizeNestedFileSpec(array $files = [])
+    {
+        $normalizedFiles = [];
+
+        foreach (array_keys($files['tmp_name']) as $key) {
+            $spec = [
+                'tmp_name' => $files['tmp_name'][$key],
+                'size' => $files['size'][$key],
+                'error' => $files['error'][$key],
+                'name' => $files['name'][$key],
+                'type' => $files['type'][$key],
+            ];
+            $normalizedFiles[$key] = self::createUploadedFileFromSpec($spec);
+        }
+
+        return $normalizedFiles;
+    }
+
+    /**
+     * Get a Uri populated with values from $swooleRequest->server.
+     * @param \Swoole\Http\Request $swooleRequest
+     * @return \Psr\Http\Message\UriInterface
+     * @throws \InvalidArgumentException
+     */
+    private static function getUriFromGlobals(\Swoole\Http\Request $swooleRequest)
+    {
+        $server = $swooleRequest->server;
+        $header = $swooleRequest->header;
+        $uri = new Uri();
+        $uri = $uri->withScheme(!empty($server['https']) && $server['https'] !== 'off' ? 'https' : 'http');
+
+        $hasPort = false;
+        if (isset($server['http_host'])) {
+            $hostHeaderParts = explode(':', $server['http_host']);
+            $uri = $uri->withHost($hostHeaderParts[0]);
+            if (isset($hostHeaderParts[1])) {
+                $hasPort = true;
+                $uri = $uri->withPort($hostHeaderParts[1]);
+            }
+        } elseif (isset($server['server_name'])) {
+            $uri = $uri->withHost($server['server_name']);
+        } elseif (isset($server['server_addr'])) {
+            $uri = $uri->withHost($server['server_addr']);
+        } elseif (isset($header['host'])) {
+            if (\strpos($header['host'], ':')) {
+                $hasPort = true;
+                list($host, $port) = explode(':', $header['host'], 2);
+
+                if ($port !== '80') {
+                    $uri = $uri->withPort($port);
+                }
+            } else {
+                $host = $header['host'];
+            }
+
+            $uri = $uri->withHost($host);
+        }
+
+        if (!$hasPort && isset($server['server_port'])) {
+            $uri = $uri->withPort($server['server_port']);
+        }
+
+        $hasQuery = false;
+        if (isset($server['request_uri'])) {
+            $requestUriParts = explode('?', $server['request_uri']);
+            $uri = $uri->withPath($requestUriParts[0]);
+            if (isset($requestUriParts[1])) {
+                $hasQuery = true;
+                $uri = $uri->withQuery($requestUriParts[1]);
+            }
+        }
+
+        if (!$hasQuery && isset($server['query_string'])) {
+            $uri = $uri->withQuery($server['query_string']);
+        }
+
+        return $uri;
+    }
+
+    public function getServerParams()
+    {
+        return $this->serverParams;
+    }
+
+    public function getCookieParams()
+    {
+        return $this->cookieParams;
+    }
+
+    public function withCookieParams(array $cookies)
+    {
+        $clone = clone $this;
+        $clone->cookieParams = $cookies;
+        return $clone;
+    }
+
+    public function getQueryParams()
+    {
+        return $this->queryParams;
+    }
+
+    public function withQueryParams(array $query)
+    {
+        $clone = clone $this;
+        $clone->queryParams = $query;
+        return $clone;
+    }
+
+    public function getUploadedFiles()
+    {
+        return $this->uploadedFiles;
+    }
+
+    public function withUploadedFiles(array $uploadedFiles)
+    {
+        $clone = clone $this;
+        $clone->uploadedFiles = $uploadedFiles;
+        return $clone;
+    }
+
+    public function getParsedBody()
+    {
+        return $this->parsedBody;
+    }
+
+    public function withParsedBody($data)
+    {
+        $clone = clone $this;
+        $clone->parsedBody = $data;
+        return $clone;
+    }
+
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
+
+    public function getAttribute($name, $default = null)
+    {
+        return array_key_exists($name, $this->attributes) ? $this->attributes[$name] : $default;
+    }
+
+    public function withAttribute($name, $value)
+    {
+        $clone = clone $this;
+        $clone->attributes[$name] = $value;
+        return $clone;
+    }
+
+    public function withoutAttribute($name)
+    {
+        if (false === array_key_exists($name, $this->attributes)) {
+            return $this;
+        }
+
+        $clone = clone $this;
+        unset($clone->attributes[$name]);
+
+        return $clone;
+    }
+
+    public function getRequestTarget()
+    {
+        if ($this->requestTarget !== null) {
+            return $this->requestTarget;
+        }
+
+        $target = $this->uri->getPath();
+        if ($target == '') {
+            $target = '/';
+        }
+        if ($this->uri->getQuery() != '') {
+            $target .= '?' . $this->uri->getQuery();
+        }
+
+        return $target;
+    }
+
+    public function withRequestTarget($requestTarget)
+    {
+        if (preg_match('#\s#', $requestTarget)) {
+            throw new \InvalidArgumentException('Invalid request target provided; cannot contain whitespace');
+        }
+
+        $new = clone $this;
+        $new->requestTarget = $requestTarget;
+        return $new;
+    }
+
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    public function withMethod($method)
+    {
+        $method = strtoupper($method);
+        $methods = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'HEAD'];
+        if (!in_array($method, $methods)) {
+            throw new \InvalidArgumentException('Invalid Method');
+        }
+        $new = clone $this;
+        $new->method = $method;
+        return $new;
+    }
+
+    public function getUri()
+    {
+        return $this->uri;
+    }
+
+    public function withUri(UriInterface $uri, $preserveHost = false)
+    {
+        if ($uri === $this->uri) {
+            return $this;
+        }
+
+        $new = clone $this;
+        $new->uri = $uri;
+
+        if (!$preserveHost) {
+            $new->updateHostFromUri();
+        }
+
+        return $new;
+    }
+
+    /**
+     * Return an instance with the specified server params.
+     *
+     * @param array $serverParams
+     * @return static
+     */
+    public function withServerParams(array $serverParams)
+    {
+        $clone = clone $this;
+        $clone->serverParams = $serverParams;
+        return $clone;
+    }
+
+    private function setHeaders(array $headers)
+    {
+        $this->headers = [];
+        foreach ($headers as $header => $value) {
+            $normalized = strtolower($header);
+            $this->headers[$normalized] = $value;
+        }
+        return $this;
+    }
+
+    private function updateHostFromUri()
+    {
+        $host = $this->uri->getHost();
+
+        if ($host === '') {
+            return;
+        }
+
+        if (($port = $this->uri->getPort()) !== null) {
+            $host .= ':' . $port;
+        }
+
+        if ($this->hasHeader('host')) {
+            $header = $this->getHeaderLine('host');
+        } else {
+            $header = 'Host';
+        }
+        // Ensure Host is the first header.
+        $this->headers = [$header => [$host]] + $this->headers;
+    }
+
+    /**
+     * @return \Swoole\Http\Request
+     */
+    public function getSwooleRequest(): \Swoole\Http\Request
+    {
+        return $this->swooleRequest;
+    }
+
+    /**
+     * @param \Swoole\Http\Request $swooleRequest
+     * @return $this
+     */
+    public function setSwooleRequest(\Swoole\Http\Request $swooleRequest)
+    {
+        $this->swooleRequest = $swooleRequest;
+        return $this;
+    }
+}
---
+++ src/Response.php
@@ -0,0 +1,210 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/10/9
+ * Time: 16:53
+ */
+
+namespace rabbit\web;
+
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+
+class Response implements ResponseInterface
+{
+    use MessageTrait;
+
+    /**
+     * @var array
+     */
+    private static $phrases = [
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-status',
+        208 => 'Already Reported',
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => 'Switch Proxy',
+        307 => 'Temporary Redirect',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Time-out',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Large',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested range not satisfiable',
+        417 => 'Expectation Failed',
+        418 => 'I\'m a teapot',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        425 => 'Unordered Collection',
+        426 => 'Upgrade Required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        451 => 'Unavailable For Legal Reasons',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Time-out',
+        505 => 'HTTP Version not supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        511 => 'Network Authentication Required',
+    ];
+
+    /**
+     * swoole响应请求
+     *
+     * @var \Swoole\Http\Response
+     */
+    protected $swooleResponse;
+
+    /**
+     * @var string
+     */
+    protected $reasonPhrase = '';
+
+    /**
+     * @var int
+     */
+    protected $statusCode = 200;
+
+    /**
+     * @var string
+     */
+    protected $charset = 'utf-8';
+
+    /**
+     * @var array
+     */
+    private $attributes = [];
+
+    /**
+     * @var array
+     */
+    protected $cookies = [];
+
+    /**
+     * 初始化响应请求
+     *
+     * @param \Swoole\Http\Response $response
+     */
+    public function __construct(\Swoole\Http\Response $response)
+    {
+        $this->swooleResponse = $response;
+    }
+
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    public function withStatus($code, $reasonPhrase = '')
+    {
+        $clone = clone $this;
+        $clone->statusCode = (int)$code;
+        if (! $reasonPhrase && isset(self::$phrases[$code])) {
+            $reasonPhrase = self::$phrases[$code];
+        }
+        $clone->reasonPhrase = $reasonPhrase;
+        return $clone;
+    }
+
+    public function getReasonPhrase()
+    {
+        return $this->reasonPhrase;
+    }
+
+    /**
+     * 处理 Response 并发送数据
+     */
+    public function send()
+    {
+        $response = $this;
+
+        /**
+         * Headers
+         */
+        // Write Headers to swoole response
+        foreach ($response->getHeaders() as $key => $value) {
+            $this->swooleResponse->header($key, implode(';', $value));
+        }
+
+        /**
+         * Cookies
+         */
+        foreach ((array)$this->cookies as $domain => $paths) {
+            foreach ($paths ?? [] as $path => $item) {
+                foreach ($item ?? [] as $name => $cookie) {
+                    if ($cookie instanceof Cookie) {
+                        $this->swooleResponse->cookie($cookie->getName(), $cookie->getValue() ? : 1, $cookie->getExpiresTime(), $cookie->getPath(), $cookie->getDomain(), $cookie->isSecure(), $cookie->isHttpOnly());
+                    }
+                }
+            }
+        }
+
+        /**
+         * Status code
+         */
+        $this->swooleResponse->status($response->getStatusCode());
+
+        /**
+         * Body
+         */
+        $this->swooleResponse->end($response->getBody()->getContents());
+    }
+
+    /**
+     * 设置Body内容，使用默认的Stream
+     *
+     * @param string $content
+     * @return static
+     */
+    public function withContent($content): Response
+    {
+        if ($this->stream) {
+            return $this;
+        }
+
+        $new = clone $this;
+        $new->stream = new SwooleStream($content);
+        return $new;
+    }
+
+    public function withCookie(Cookie $cookie)
+    {
+        $clone = clone $this;
+        $clone->cookies[$cookie->getDomain()][$cookie->getPath()][$cookie->getName()] = $cookie;
+        return $clone;
+    }
+    
+    
+}
