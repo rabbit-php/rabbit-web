6966de4bab0b3126fe9c7f7e7ff2e0c78d911a7f
albert <amuluowin@163.com>
2020-07-07 09:51:31 +0800

    add file

---
+++ src/AttributeEnum.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace Rabbit\Web;
+
+/**
+ * Class AttributeEnum
+ * @package Rabbit\Server
+ */
+class AttributeEnum
+{
+    /**
+     * The attribute of Router
+     *
+     * @var string
+     */
+    const ROUTER_ATTRIBUTE = 'requestHandler';
+
+    /**
+     * The attribute of response data
+     *
+     * @var string
+     */
+    const RESPONSE_ATTRIBUTE = 'responseAttribute';
+
+    /**
+     * The attribute of requesId
+     *
+     * @var string
+     */
+    const REQUESTID_ATTRIBUTE = 'requestId';
+
+    /**
+     * The attribute of connectFd
+     *
+     * @var int
+     */
+    const CONNECT_FD = 'connectFd';
+}
