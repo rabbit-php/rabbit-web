3f5e14584972d6e24c4060eed89d00ba743e0356
Albert <amuluowin@163.com>
2021-01-20 20:27:24 +0800

    update

--- src/RequestHandlerInterface.php
+++ src/RequestHandlerInterface.php
@@ -1,15 +1,12 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Rabbit\Web;
 
 use Psr\Http\Message\ServerRequestInterface;
 
-/**
- * Interface RequestHandlerInterface
- * @package rabbit\server
- */
 interface RequestHandlerInterface
 {
-    public function __invoke(array $params = [], ServerRequestInterface $request = null);
+    public function __invoke(ServerRequestInterface $request);
 }
